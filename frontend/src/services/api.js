4486428be06492b500e13bf65c95cdd99506286c
richtg22 <tridishag479@gmail.com>
2026-05-30 19:54:06 +0530

    Connected frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+export default axios.create({
+  baseURL: "https://ai-client-communication-hub-backend.onrender.com",
 });
-
-export default api;
